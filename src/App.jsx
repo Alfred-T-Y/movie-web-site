dcae1f63fb0a18c4b8ebf27937ca72ccdb92ed05
Alfred-T-Y <tchoppeyonaalfred@gmail.com>
2025-10-07 22:06:43 +0100

    add: updateSearchCount added

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,9 @@ const App = () => {
 
       setmovieList(data.results || []);
 
-      updateSearchCount();
+      if(query && data.results.length > 0){
+        await updateSearchCount(query, data.results[0]);
+      }
  
     } catch (error){
       console.error(`Error fetching movies: ${error}`);
--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "appwrite";
+import { Client, Query, TablesDB, ID } from "appwrite";
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
@@ -9,14 +9,42 @@ const client = new Client()
     .setEndpoint(ENDPOINT)
     .setProject(PROJECT_ID)
 
-const database = new Databases(client)
+const database = new TablesDB(client)
 
 export const updateSearchCount = async(searchTerm, movie) => {
 
     try{
-        const result = await database.listDocuments
-    } catch() {
+        const result = await database.listRows({
+            databaseId : DATABASE_ID,
+            tableId : TABLE_ID,
+            queries : [ Query.equal("searchTerm", searchTerm) ]
+        })
 
+        if(result.total < 1){
+            await database.createRow({
+                databaseId: DATABASE_ID,
+                tableId: TABLE_ID,
+                rowId: ID.unique(), 
+                data: {
+                 searchTerm,
+                 count: 1,
+                 movie_id: movie.id,
+                 poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                }
+            })
+        } else {
+            const doc = result.rows[0];
+            await database.updateRow(
+                DATABASE_ID, 
+                TABLE_ID, 
+                doc.$id, 
+                {
+                 count: doc.count + 1,
+                }
+            )
+        }
+    } catch(error) {
+        console.error(error);
     }
 
 }
